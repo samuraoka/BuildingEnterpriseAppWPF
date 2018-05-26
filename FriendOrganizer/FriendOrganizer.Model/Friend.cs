9813aa37f1c5d92491968d3870479840d8495d5b
Satoshi Muraoka <sa.muraoka@outlook.com>
2018-05-27 08:34:04 +0900

    Add a model for this solution.

---
+++ FriendOrganizer/FriendOrganizer.Model/Friend.cs
@@ -0,0 +1,10 @@
+﻿namespace FriendOrganizer.Model
+{
+    public class Friend
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+    }
+}
